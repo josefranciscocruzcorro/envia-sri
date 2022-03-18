19f1322ab980de1f19f26f1a5c38d4d4ebbb80e5
Jose Francisco Cruz Corro <josefranciscocruzcorro@gmail.com>
2022-03-17 22:44:05 -0500

    version estable 1

---
+++ src/Enviador.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace Josefranciscocruzcorro\EnviaSri;
+
+use nusoap_client;
+
+class Enviador
+{
+    public static function enviar($xml,$ambiente)
+    {
+        # code...
+        $mensaje = base64_encode($xml);
+        $parametros = array(); 
+        $parametros['xml'] = $mensaje;
+
+        $servicio = "https://celcer.sri.gob.ec/comprobantes-electronicos-ws/RecepcionComprobantesOffline?wsdl";
+        if ($ambiente == 2) {
+            # code...
+            $servicio = "https://cel.sri.gob.ec/comprobantes-electronicos-ws/RecepcionComprobantesOffline?wsdl";
+        }
+
+        $client = new nusoap_client($servicio);
+        $client->soap_defencoding = 'utf-8';
+
+        $result = $client->call("validarComprobante", $parametros, "http://ec.gob.sri.ws.recepcion");
+
+
+        
+        return $result["comprobantes"]["comprobante"]["mensajes"]["mensaje"]["mensaje"];
+        
+    }
+    
+    public static function autorizar($claveAcceso,$ambiente)
+    {
+        # code...
+        $parametros = array(); 
+        $parametros['claveAccesoComprobante'] = $claveAcceso;
+
+        $servicio = "https://celcer.sri.gob.ec/comprobantes-electronicos-ws/AutorizacionComprobantesOffline?wsdl"; 
+        if ($ambiente == 2) {
+            # code...
+            $servicio = "https://cel.sri.gob.ec/comprobantes-electronicos-ws/AutorizacionComprobantesOffline?wsdl"; 
+        }
+
+        $client = new nusoap_client($servicio);
+        $client->soap_defencoding = 'utf-8';
+
+        $result = $client->call("autorizacionComprobante", $parametros, "http://ec.gob.sri.ws.autorizacion");
+
+        if (@$result['autorizaciones']["autorizacion"]) {
+            # code...
+            return $result['autorizaciones']["autorizacion"]["estado"];
+        }
+
+        return 'NO EXISTE';
+    }
+
+    public static function extraerComprobanteJSON($claveAcceso,$ambiente)
+    {
+        # code...
+        $parametros = array(); 
+        $parametros['claveAccesoComprobante'] = $claveAcceso;
+
+        $servicio = "https://celcer.sri.gob.ec/comprobantes-electronicos-ws/AutorizacionComprobantesOffline?wsdl"; 
+        if ($ambiente == 2) {
+            # code...
+            $servicio = "https://cel.sri.gob.ec/comprobantes-electronicos-ws/AutorizacionComprobantesOffline?wsdl"; 
+        }
+
+        $client = new nusoap_client($servicio);
+        $client->soap_defencoding = 'utf-8';
+
+        $result = $client->call("autorizacionComprobante", $parametros, "http://ec.gob.sri.ws.autorizacion");
+
+        $r2 = '';
+        if (@$result['autorizaciones']["autorizacion"]) {
+            # code...
+            $r2 = $result['autorizaciones']["autorizacion"]["comprobante"];
+
+            $xml = simplexml_load_string($r2);
+            $r2 = json_encode($xml);
+        }
+
+        return $r2;
+    }
+}
