fcaadbb73b966a6d640ddc5b17cc595139abec28
Jose Francisco Cruz Corro <josefranciscocruzcorro@gmail.com>
2022-04-04 18:48:05 -0500

    ss

--- src/Enviador.php
+++ src/Enviador.php
@@ -88,4 +88,37 @@ class Enviador
 
         return $r2;
     }
+
+    public static function extraerComprobanteXML($claveAcceso,$ambiente)
+    {
+        # code...
+        $parametros = array(); 
+        $parametros['claveAccesoComprobante'] = $claveAcceso;
+
+        $servicio = "https://celcer.sri.gob.ec/comprobantes-electronicos-ws/AutorizacionComprobantesOffline?wsdl"; 
+        if ($ambiente == 2) {
+            # code...
+            $servicio = "https://cel.sri.gob.ec/comprobantes-electronicos-ws/AutorizacionComprobantesOffline?wsdl"; 
+        }
+
+        $client = new nusoap_client($servicio);
+        $client->soap_defencoding = 'utf-8';
+
+        $result = $client->call("autorizacionComprobante", $parametros, "http://ec.gob.sri.ws.autorizacion");
+
+        $xml = null;
+        if (@$result['autorizaciones']["autorizacion"]) {
+            # code...
+            $r2 = $result['autorizaciones']["autorizacion"]["comprobante"];
+
+            //$r2 = utf8_encode($r2);
+            $r2 = utf8_decode($r2);
+            $r2 = str_replace("&lt;","<",$r2);
+            $r2 = str_replace("&gt;",">",$r2);
+
+            $xml = simplexml_load_string($r2);
+        }
+
+        return $xml;
+    }
 }
